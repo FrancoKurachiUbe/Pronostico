3ef4fce51f8ef10af183b1c1ba95f7f49589ddf2
Franco <83045373+FrancoKurachiUbe@users.noreply.github.com>
2021-06-24 16:07:53 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-const API_KEY = //;
+const API_KEY_MAPS = 'AIzaSyDJoSnyar51P-hzKGy_ahuS7FAB-N3vG2M';
+const URL_MAPS = 'https://www.google.com/maps/embed/v1/place';
+
+const API_KEY = '7ea38955d61db8cda00f1b7e8b8fa59c';
 const URL = 'http://api.openweathermap.org/data/2.5/';
 
 
 const button = document.getElementById("sendButton");
 const main = document.getElementById("dato");
 const inputElement = document.getElementById("search");
+const mapa = document.getElementById("mapa");
  
 button.addEventListener("click", ()=>{
   searchClima(inputElement.value);
 });
 
-
 function searchClima(wordToSearch){
     const fetchPromise = fetch(`${URL}weather?q=${wordToSearch},&APPID=${API_KEY}`);
+    
     
     fetchPromise.then(response =>{
         console.log('result', response);
@@ -28,10 +32,13 @@ function searchClima(wordToSearch){
 function madeGrid(result){
   const nombre = result.name;
   console.log('nombre',nombre);
+  localStorage.getItem("nombre");
+  const mapa = `${URL_MAPS}?key=${API_KEY_MAPS}&q=${nombre}`;
+  console.log(mapa);
   //------Temperatura-------
   const temp = Math.round(result.main.temp - 273.15);
   //console.log('temperatura',temp);
-
+  
   //-------Temperatura Maxima-------
   const temp_max = Math.round(result.main.temp_max - 273.15);
   //console.log('temperatura Maxima', temp_max);
@@ -44,48 +51,65 @@ function madeGrid(result){
   const humidity = result.main.humidity;  
  
   //-------Sensacion Termica-------
-  const feels_like = result.main.feels_like;
+  const feels_like = Math.round(result.main.feels_like -273.15);
 
   //-------Precion-------
   const pressure = result.main.pressure;
    
   //------Velocidad del Viento------
   let speed = result.wind.speed;
-  console.log('La Velocidad es de: ', speed);
-  //--------------Clima------------
-  let clima = result.weather.map(img => img.main);
-  let cielo = 0;
-  console.log(clima);
+  //console.log('La Velocidad es de: ', speed);
 
-    if(clima == 'Clear'){
-      clima = 'sol';
-    }else if(clima == 'Clouds'){
-      clima = 'nube';
-    }
+  //--------------icono------------
   
-    if(clima == 'sol'){
-      cielo = 'Despejado';
-    }else{
-      cielo = 'nublado';
-    }
+  let icono = result.weather.map(ico => ico.icon);
+  //console.log('icono',icono);
+  
+  //--------------Clima------------
+  let cielo = result.weather.map(img => img.description);
+  //console.log('cielo',cielo);
+ 
+  let clima = result.weather.map(cli => cli.main);
+  //console.log('clima',clima);
+    
+    if(clima == 'Clear'){
+        clima = 'sol';
+      }else{
+        clima = 'nube';
+      }
 
   dato.innerHTML =
   `
-  <div class=" ${clima} row container border mx-auto pb-4">
-  <section class=" col-md-7 mx-auto mt-4 pb-3">
-      <p class="text-center">${nombre}</p>
-      <div class=" text-center">
-          <img class="align-top mr-4 " src="img/${clima}_Pc.png" alt="${clima}">
-          <p class="ml-4 d-inline-block grados">${temp}°</p>
-      </div>
-      <p class="text-center">${temp_min}°/${temp_max}° Sensacíon Térmica ${feels_like}°</p>
-      <p class="text-center">${cielo}</p>
-      <div class="detalle detalle-${clima} rounded pl-2 ">
-          <p>Humedad: ${humidity}%</p>
-          <p>Precion: ${pressure} hPa</p>
-          <p>Velocidad del viento: ${speed} km/h</p>
-      </div>
-  </section>
-</div>
+<div class="${clima} contenedor">
+        <section class="">
+            <p class="centrar">${nombre}</p>
+            <div class="contenedor2 detalle-${clima}">
+                  <img src="http://openweathermap.org/img/wn/${icono}@2x.png" alt="${cielo}">  
+                <p>${temp}°</p>
+            </div>
+            
+            
+            <div class="detalle ${clima}">
+                  <div>
+                    <p>Temperatura minima ${temp_min}°</p>
+                    <p>Temperatura maxima ${temp_max}°</p>
+                    <p>Sensacíon Térmica ${feels_like}°</p>
+           
+                
+                    <p>Humedad: ${humidity}%</p>
+                    <p>Precion: ${pressure} hPa</p>
+                    <p>Vel. del viento: ${speed} km/h</p>
+                  </div>
+            </div>
+            <div class="mapa">
+                <iframe
+                width="250"
+                height="250"
+                frameborder="0" style="border:0" shape="circle"
+                src="${mapa}" allowfullscreen>
+                </iframe>
+            </div>
+        </section>
+    </div>
   `
 }
